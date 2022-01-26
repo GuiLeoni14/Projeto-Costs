028ae8e8ddbb385b7d50b3390392fb411d6bfc21
GuiLeoni14 <guileone167@gmail.com>
2022-01-26 17:25:25 -0200

    Criando a home

---
+++ src/components/layout/Buttons/LinkButton.jsx
@@ -0,0 +1,15 @@
+import P from 'prop-types';
+import style from './styles.scss';
+import { Link } from 'react-router-dom';
+export function LinkButton({ to, textButton }) {
+    return (
+        <Link to={to} className="btn btn_link">
+            {textButton}
+        </Link>
+    );
+}
+
+LinkButton.propTypes = {
+    textButton: P.string.isRequired,
+    to: P.string.isRequired,
+};
--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,11 +1,20 @@
 import Container from '../../layout/Container';
+import savings from '../../../assets/img/savings.svg';
 import './styles.scss';
+import { LinkButton } from '../../layout/Buttons/LinkButton';
 export default function Home() {
     return (
         <section className="s-home">
             <Container customClass="min_height">
                 <div className="main-home">
-                    <h1>Home</h1>
+                    <div className="text">
+                        <h1>
+                            Bem vindo ao <span>Costs</span>
+                        </h1>
+                        <p>Comece a gerenciar seus projetos agora mesmo!</p>
+                        <LinkButton to="/newproject" textButton="Criar Projeto" />
+                        <img src={savings} alt="costs" />
+                    </div>
                 </div>
             </Container>
         </section>
