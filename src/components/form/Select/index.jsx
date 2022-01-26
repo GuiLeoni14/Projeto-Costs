846393283319397f6430e7a33fb219561a99cb10
GuiLeoni14 <guileone167@gmail.com>
2022-01-26 21:07:04 -0200

    Conectando com API pelo React

--- src/components/form/Select/index.jsx
+++ src/components/form/Select/index.jsx
@@ -1,6 +1,7 @@
 import P from 'prop-types';
 import './styles.scss';
 export default function Select({ text, name, options, handleOnChange, value }) {
+    console.log(options);
     return (
         <div className="form_control">
             <label htmlFor={name}>{text}:</label>
@@ -8,6 +9,12 @@ export default function Select({ text, name, options, handleOnChange, value }) {
                 <option disabled selected>
                     Selecione uma opção:
                 </option>
+                {options.length > 0 &&
+                    options.map((categoria) => (
+                        <option value={categoria.id} key={categoria.id}>
+                            {categoria.name}
+                        </option>
+                    ))}
             </select>
         </div>
     );
@@ -16,7 +23,7 @@ export default function Select({ text, name, options, handleOnChange, value }) {
 Select.propTypes = {
     text: P.string.isRequired,
     name: P.string.isRequired,
-    options: P.object,
+    options: P.array,
     handleOnChange: P.func,
     value: P.func,
 };
--- src/components/project/ProjectForm/index.jsx
+++ src/components/project/ProjectForm/index.jsx
@@ -2,13 +2,22 @@ import { SubmitButton } from '../../Buttons/SubmitButton';
 import Input from '../../Form/Input';
 import Select from '../../form/select';
 import P from 'prop-types';
-
+import { useEffect, useState } from 'react';
 export default function ProjectForm({ btnText }) {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        const fetchApi = async () => {
+            const response = await fetch('http://localhost:5000/categories');
+            const data = await response.json();
+            setCategories(data);
+        };
+        fetchApi();
+    }, []);
     return (
         <form action="">
             <Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do Projeto" />
             <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o Orçamento total" />
-            <Select text="Selecione uma Categoria" name="category_id" />
+            <Select text="Selecione uma Categoria" name="category_id" options={categories} />
             <SubmitButton textButton={btnText} />
         </form>
     );
